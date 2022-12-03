98949892175f7358cf9b6f20a6dbe9dfbe1137d6
Willian <92097335+Will448@users.noreply.github.com>
2022-12-03 18:23:50 -0300

    Delete Reformulacao-TI-2.0 directory

--- Reformulacao-TI-2.0/README.md
+++
@@ -1,3 +0,0 @@
-# TI-CRUD-Login
-Link para o ReCaptcha: https://www.google.com/recaptcha/admin/site/588061427
-https://console.firebase.google.com/project/teste-ceb46/overview
--- Reformulacao-TI-2.0/bd/auth-guard.js
+++
@@ -1,8 +0,0 @@
-firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-        swal("Good job!", "You clicked the button!", "success");
-        window.location.href = "../../index.html";
-        
-
-    }
-})
